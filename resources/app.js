0889819f23cf6b02499a12cc081605cb9d17dd1d
Your Name <your_email@example.com>
2025-09-29 22:28:17 +0100

    add fullscreen feature

--- resources/app.js
+++ resources/app.js
@@ -99,6 +99,8 @@ window.addEventListener('load', ()=>{
   previewBtn.classList.add("btn-primary");
 });
 
+
+// vvvvvvvvvvvv  here  vvvvvvvvvvvv
 const fullScreenBtn= document.querySelector('#fullScreenBtn');
 fullScreenBtn.addEventListener('click', ()=>{
   let innerSvg= null;
@@ -117,6 +119,7 @@ document.addEventListener('fullscreenchange', ()=>{
                                                                   ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M3 21v-5h2v3h3v2zm13 0v-2h3v-3h2v5zM3 8V3h5v2H5v3zm16 0V5h-3V3h5v5z"/></svg>`
                                                                   : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 21v-3H3v-2h5v5zm10 0v-5h5v2h-3v3zM3 8V6h3V3h2v5zm13 0V3h2v3h3v2z"/></svg>` ;
 });
+//^^^^^^^^^^^^^^^^^^^^^^^
 
 const noteShareBtn= document.querySelector('#noteShareBtn');
 const noteShareBtnSvgContainer= document.querySelector('#noteShareBtnSvgContainer');
