c984f28d2fd26e1339593a341d192d72c528418c
Your Name <your_email@example.com>
2025-09-29 01:20:16 +0100

    add preview button to check rendred markdown

--- resources/app.js
+++ resources/app.js
@@ -1,4 +1,3 @@
-const textarea = document.getElementById("noteContent");
 
 // dynamic assignment of heading
 const dropdownButton = document.querySelector('.dropdown-toggle'); 
@@ -17,23 +16,7 @@ dropdownItems.forEach(item => {
 
 
 
-// link upload dynamic width
-document.querySelectorAll(".dynamic-width").forEach(input => {
-    const baseWidth = 50; // min width in pixels
-    const perChar = 25;    // width per character (approx)
-
-    function resize() {
-      const length = input.value.length;
-      input.style.width = Math.max(baseWidth, length * perChar) + "px";
-    }
-
-    input.addEventListener("input", resize);
-    resize(); // initialize on page load
-  });
-
-
-
-
+const textarea = document.getElementById("noteContent");
   // auto height on textarea input
 textarea.addEventListener("input", function () {
 this.style.height = "auto";            // reset height
@@ -57,12 +40,43 @@ const addLinkBtn= document.querySelector("#addLinkBtn");
 const addLinkLayout= document.querySelector("#addLinkLayout");
 let addLinkLayoutVisibility= false;
 
-window.addEventListener('load', ()=>{
-  addLinkLayout.hidden= true;
-});
 
 addLinkBtn.addEventListener('click', function(){
   addLinkLayoutVisibility = !addLinkLayoutVisibility;
   addLinkLayout.hidden= !addLinkLayoutVisibility;
 });
 
+const notePreview= document.querySelector('#notePreview');
+const previewBtn= document.querySelector("#previewBtn");
+const previewBtnSvgPreviewOn= document.querySelector('#previewBtnSvgPreviewOn');
+const previewBtnSvgPreviewOff= document.querySelector('#previewBtnSvgPreviewOff');
+
+let hidePreview= true;
+previewBtn.addEventListener('click', ()=>{
+
+  hidePreview= !hidePreview;
+  notePreview.hidden= hidePreview;
+  textarea.hidden= !notePreview.hidden;
+  console.log(hidePreview);
+  let btn_classes={
+    'hidden': "btn-secondary",
+    'not_hidden': "btn-primary"
+  };
+  let current_class= null;
+  for (const css_class of previewBtn.classList){
+    if(css_class.startsWith('btn-')){
+      current_class= css_class;
+      break;
+    }
+  };
+  
+  let btn_class= current_class === btn_classes.hidden ? btn_classes.not_hidden : btn_classes.hidden;
+  previewBtn.classList.replace(current_class, btn_class);
+  
+});
+
+window.addEventListener('load', ()=>{
+  addLinkLayout.hidden= true;
+  notePreview.hidden= true;
+  previewBtn.classList.add("btn-primary");
+});
