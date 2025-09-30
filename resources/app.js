8e70c6f6951fce6fa6afcd15d606c73432992689
Your Name <your_email@example.com>
2025-09-30 01:56:53 +0100

    addLink layout and fix toggling logic

--- resources/app.js
+++ resources/app.js
@@ -89,3 +89,14 @@ document.addEventListener('fullscreenchange', ()=>{
                                                                   ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M3 21v-5h2v3h3v2zm13 0v-2h3v-3h2v5zM3 8V3h5v2H5v3zm16 0V5h-3V3h5v5z"/></svg>`
                                                                   : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 21v-3H3v-2h5v5zm10 0v-5h5v2h-3v3zM3 8V6h3V3h2v5zm13 0V3h2v3h3v2z"/></svg>` ;
 });
+
+
+const addLinkBtn= document.querySelector("#addLinkBtn");
+const addLinkLayout= document.querySelector("#addLinkLayout");
+let addLinkLayoutVisibility= false;
+
+
+addLinkBtn.addEventListener('click', function(){
+  addLinkLayoutVisibility = !addLinkLayoutVisibility;
+  addLinkLayout.hidden= !addLinkLayoutVisibility;
+});
